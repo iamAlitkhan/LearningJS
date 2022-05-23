5f555fd33d876d49d9c6ede8daad844dbfadad46
iamAlitkhan <khan.alitanveer@gmail.com>
2022-05-23 13:19:07 -0500

    adding JS assignment

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+let friends = ["John", "Mary", "Steven", "Bill"]
+
+for (let index = 0; index < friends.length; index++) {
+    console.log(friends[index])
+}
+
+
+let number1 = 50
+let number2 = 31
+
+let total = number1 + number2
+
+if (total >= 50 && total <= 80) {
+  console.log(65)
+} else {
+  console.log(80)
+}
